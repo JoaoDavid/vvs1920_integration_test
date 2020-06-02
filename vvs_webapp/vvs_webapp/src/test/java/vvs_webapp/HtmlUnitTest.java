85efc02b15b3fd5e98bb4fc46e69a4bf49110338
M44TRIX <joaodavid512@gmail.com>
2020-06-03 00:06:12 +0100

    added html unit test class

---
+++ vvs_webapp/vvs_webapp/src/test/java/vvs_webapp/HtmlUnitTest.java
@@ -0,0 +1,61 @@
+package vvs_webapp;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
+
+import java.net.MalformedURLException;
+
+import java.io.*;
+import java.util.*;
+
+public class HtmlUnitTest {
+	
+	private static final String APPLICATION_URL = "http://localhost:8080/VVS_webappdemo/";
+	private static final int APPLICATION_NUMBER_USE_CASES = 11;
+
+	private static HtmlPage page;
+	private static TestUtil util;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		try (final WebClient webClient = new WebClient(BrowserVersion.getDefault())) { 
+	
+			// possible configurations needed to prevent JUnit tests to fail for complex HTML pages
+            webClient.setJavaScriptTimeout(15000);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.getOptions().setCssEnabled(false);
+            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+		    
+			page = webClient.getPage(APPLICATION_URL);
+			util = new TestUtil(page);
+			
+			assertEquals(200, page.getWebResponse().getStatusCode()); // OK status
+		}
+	}
+	
+	@Test
+	public void addCustomerTest() throws Exception {
+		final String vat = "264639405";
+        util.addCustomer(vat, "batata", "918576089");
+        util.addAddress(vat, "rua da batata", "9", "4242-225", "lisboa");
+	}
+	
+
+
+}
+
+
+
+
+
