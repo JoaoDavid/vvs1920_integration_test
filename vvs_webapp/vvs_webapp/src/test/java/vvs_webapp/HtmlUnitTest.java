2fd2732721515e9c680afc0234384288f81ef5ef
M44TRIX <joaodavid512@gmail.com>
2020-06-04 17:12:42 +0100

    narrativeA and B ok

--- vvs_webapp/vvs_webapp/src/test/java/vvs_webapp/HtmlUnitTest.java
+++ vvs_webapp/vvs_webapp/src/test/java/vvs_webapp/HtmlUnitTest.java
@@ -5,74 +5,55 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 
 public class HtmlUnitTest {
 
 	private static final String APPLICATION_URL = "http://localhost:8080/VVS_webappdemo/";
-	private static final int APPLICATION_NUMBER_USE_CASES = 11;
 
-	private static HtmlPage page;
-	private static TestUtil util;
+	private WebClient webClient;
+	private TestUtil util;
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-		try (final WebClient webClient = new WebClient(BrowserVersion.getDefault())) { 
-
-			// possible configurations needed to prevent JUnit tests to fail for complex HTML pages
-			webClient.setJavaScriptTimeout(15000);
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-			webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-			webClient.getOptions().setCssEnabled(false);
-			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-			webClient.getOptions().setJavaScriptEnabled(true);
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-
-			page = webClient.getPage(APPLICATION_URL);
-			util = new TestUtil(page);
-
-			assertEquals(200, page.getWebResponse().getStatusCode()); // OK status
-		}
+	@Before
+	public void setUpClass() throws Exception {
+		webClient = new WebClient();
+		util = new TestUtil(webClient);
+	}
+	
+	@After
+	public void tearDownClass() throws Exception {
+		webClient.close();
 	}
 
-	@Test
+	//@Test
 	public void myTest() throws Exception {
 		final String vat = "244377090";
 		util.addCustomer(vat, "batata assada", "918576089");
 		util.addAddress(vat, "rua da batata", "9", "4242-225", "lisboa");
-		/*final HtmlTable tableAfter = util.getCustomerAddresses(vat);
-		final int numRowsAfter = tableAfter.getRowCount();
-		//Verify row count increased by 2
-		//Verify that the table of addresses includes the new ones
+		HtmlTable tableAfter = util.getCustomerAddresses(vat);
 		for (final HtmlTableRow row : tableAfter.getRows()) {
 			System.out.println(row.asText());
-		}*/
+		}
 		util.addSale(vat);
 		//util.removeCustomer(vat);
 	}
 
-	//@Test
+	@Test
 	public void narrativeA() throws IOException {
 		//Set Up
 		final String vat = "244377090";
 		final String desig = "José";
 		final String phone = "910576931";
 		util.addCustomer(vat, desig, phone);
-		int confirmedInfoCount = 0;
 		//State of the table before adding the 2 new addresses
 		final HtmlTable tableBefore = util.getCustomerAddresses(vat);
-		final int numRowsBefore = tableBefore.getRowCount();
+		final int numRowsBefore = tableBefore==null ? 1 : tableBefore.getRowCount();
 		//Adding the 2 addresses
 		final String address1 = "Rua Augusta";
 		final String door1 = "9";
@@ -90,27 +71,22 @@ public class HtmlUnitTest {
 		//Verify row count increased by 2
 		assertEquals(2, numRowsAfter-numRowsBefore);
 		//Verify that the table of addresses includes the new ones
-		for (final HtmlTableRow row : tableAfter.getRows()) {
-			if (row.getCell(0).asText().equals(address1) 
-					&& row.getCell(1).asText().equals(door1)
-					&& row.getCell(2).asText().equals(postalCode1)
-					&& row.getCell(3).asText().equals(locality1)) {
-				confirmedInfoCount++;
-			} else if (row.getCell(0).asText().equals(address2) 
-					&& row.getCell(1).asText().equals(door2)
-					&& row.getCell(2).asText().equals(postalCode2)
-					&& row.getCell(3).asText().equals(locality2)) {
-				confirmedInfoCount++;
-			}
-		}
-		assertEquals(2, confirmedInfoCount);
+		final HtmlTableRow fstAddedRow = tableAfter.getRow(numRowsAfter-2);
+		assertEquals(address1, fstAddedRow.getCell(0).asText());
+		assertEquals(door1, fstAddedRow.getCell(1).asText());
+		assertEquals(postalCode1, fstAddedRow.getCell(2).asText());
+		assertEquals(locality1, fstAddedRow.getCell(3).asText());
+		final HtmlTableRow sndAddedRow = tableAfter.getRow(numRowsAfter-1);
+		assertEquals(address2, sndAddedRow.getCell(0).asText());
+		assertEquals(door2, sndAddedRow.getCell(1).asText());
+		assertEquals(postalCode2, sndAddedRow.getCell(2).asText());
+		assertEquals(locality2, sndAddedRow.getCell(3).asText());
 		// Tear down
 		util.removeCustomer(vat);
 	}
 
-	//@Test
+	@Test
 	public void narrativeB() throws IOException {
-		HtmlAnchor getCustomersLink = page.getAnchorByHref("GetAllCustomersPageController");
 		String vat1 = "244377090";
 		String desig1 = "José";
 		String phone1 = "910576931";
@@ -119,15 +95,13 @@ public class HtmlUnitTest {
 		String phone2 = "960872610";
 		int confirmedInfoCount = 0;
 		//-------------before---------
-		HtmlPage nextPageBefore = (HtmlPage) getCustomersLink.openLinkInNewWindow();		
-		final HtmlTable tableBefore = nextPageBefore.getHtmlElementById("clients");
-		int countBefore = tableBefore.getRowCount();
+		final HtmlTable tableBefore = util.getCustomers();
+		final int countBefore = tableBefore==null ? 1 : tableBefore.getRowCount();
 		//------------add customers---------
 		util.addCustomer(vat1, desig1, phone1);
 		util.addCustomer(vat2, desig2, phone2);
 		//-------------after---------
-		HtmlPage nextPageAfter = (HtmlPage) getCustomersLink.openLinkInNewWindow();		
-		final HtmlTable tableAfter = nextPageAfter.getHtmlElementById("clients");
+		final HtmlTable tableAfter = util.getCustomers();
 		int countAfter = tableAfter.getRowCount();
 		for (final HtmlTableRow row : tableAfter.getRows()) {
 			if (row.getCell(2).asText().equals(vat1)) {
@@ -148,6 +122,17 @@ public class HtmlUnitTest {
 		// Tear down
 		util.removeCustomer(vat1);
 		util.removeCustomer(vat2);
+	}
+
+	//@Test
+	public void narrativeC() throws IOException {
+		//Set Up
+		final String vat = "244377090";
+		final String desig = "José";
+		final String phone = "910576931";
+		util.addCustomer(vat, desig, phone);
+		//Add sale
+		util.addSale(vat);
 	}
 
 
--- vvs_webapp/vvs_webapp/src/test/java/vvs_webapp/TestUtil.java
+++ vvs_webapp/vvs_webapp/src/test/java/vvs_webapp/TestUtil.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -15,124 +13,145 @@ import com.gargoylesoftware.htmlunit.html.HtmlTable;
 
 public class TestUtil {
 
-	private HtmlPage page;
+	private static final String APPLICATION_URL = "http://localhost:8080/VVS_webappdemo/";
 
-	public TestUtil(HtmlPage page) {
-		this.page = page;
+	private WebClient webClient;	
+	
+	public TestUtil(WebClient webClient) {
+		this.webClient = webClient;
 	}
 
 	public void addCustomer(String vat, String desig, String phone) throws IOException {
-		// get a specific link
-		HtmlAnchor addCustomerLink = page.getAnchorByHref("addCustomer.html");
-		// click on it
-		HtmlPage nextPage = (HtmlPage) addCustomerLink.openLinkInNewWindow();
-		// check if title is the one expected
-		assertEquals("Enter Name", nextPage.getTitleText());
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage page = webClient.getPage(APPLICATION_URL + "addCustomer.html");
+			// check if title is the one expected
+			assertEquals("Enter Name", page.getTitleText());
 
-		// get the page first form:
-		HtmlForm addCustomerForm = nextPage.getForms().get(0);
+			// get the page first form:
+			HtmlForm addCustomerForm = page.getForms().get(0);
 
-		// place data at form
-		HtmlInput vatInput = addCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(vat);
-		HtmlInput designationInput = addCustomerForm.getInputByName("designation");
-		designationInput.setValueAttribute(desig);
-		HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
-		phoneInput.setValueAttribute(phone);
-		// submit form
-		HtmlInput submit = addCustomerForm.getInputByName("submit");
+			// place data at form
+			HtmlInput vatInput = addCustomerForm.getInputByName("vat");
+			vatInput.setValueAttribute(vat);
+			HtmlInput designationInput = addCustomerForm.getInputByName("designation");
+			designationInput.setValueAttribute(desig);
+			HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
+			phoneInput.setValueAttribute(phone);
+			// submit form
+			HtmlInput submit = addCustomerForm.getInputByName("submit");
 
-		// check if report page includes the proper values
-		HtmlPage reportPage = submit.click();
-		String textReportPage = reportPage.asText();
-		assertTrue(textReportPage.contains(vat));
-		assertTrue(textReportPage.contains(desig));
-		assertTrue(textReportPage.contains(phone));
+			// check if report page includes the proper values
+			HtmlPage reportPage = submit.click();
+			String textReportPage = reportPage.asText();
+			assertTrue(textReportPage.contains(desig));
+			assertTrue(textReportPage.contains(phone));
+		//}
 	}
-	
+
 	public void addAddress(String vat, String address, String door, String postalCode, String locality) throws IOException {
-		// get a specific link
-		HtmlAnchor addAddressLink = page.getAnchorByHref("addAddressToCustomer.html");
-		// click on it
-		HtmlPage nextPage = (HtmlPage) addAddressLink.openLinkInNewWindow();
-		// check if title is the one expected
-		assertEquals("Enter Address", nextPage.getTitleText());
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage nextPage = webClient.getPage(APPLICATION_URL + "addAddressToCustomer.html");
+			// check if title is the one expected
+			assertEquals("Enter Address", nextPage.getTitleText());
 
-		// get the page first form:
-		HtmlForm addAddressForm = nextPage.getForms().get(0);
+			// get the page first form:
+			HtmlForm addAddressForm = nextPage.getForms().get(0);
 
-		// place data at form
-		HtmlInput vatInput = addAddressForm.getInputByName("vat");
-		vatInput.setValueAttribute(vat);
-		HtmlInput addressInput = addAddressForm.getInputByName("address");
-		addressInput.setValueAttribute(address);
-		HtmlInput doorInput = addAddressForm.getInputByName("door");
-		doorInput.setValueAttribute(door);
-		HtmlInput postalCodeInput = addAddressForm.getInputByName("postalCode");
-		postalCodeInput.setValueAttribute(postalCode);
-		HtmlInput localityInput = addAddressForm.getInputByName("locality");
-		localityInput.setValueAttribute(locality);
-		// submit form
-		HtmlInput submit = addAddressForm.getInputByValue("Insert");
+			// place data at form
+			HtmlInput vatInput = addAddressForm.getInputByName("vat");
+			vatInput.setValueAttribute(vat);
+			HtmlInput addressInput = addAddressForm.getInputByName("address");
+			addressInput.setValueAttribute(address);
+			HtmlInput doorInput = addAddressForm.getInputByName("door");
+			doorInput.setValueAttribute(door);
+			HtmlInput postalCodeInput = addAddressForm.getInputByName("postalCode");
+			postalCodeInput.setValueAttribute(postalCode);
+			HtmlInput localityInput = addAddressForm.getInputByName("locality");
+			localityInput.setValueAttribute(locality);
+			// submit form
+			HtmlInput submit = addAddressForm.getInputByValue("Insert");
 
-		// check if report page includes the proper values
-		HtmlPage reportPage = submit.click();
-		String textReportPage = reportPage.asText();
-		assertTrue(textReportPage.contains(vat));
-		assertTrue(textReportPage.contains(address));
-		assertTrue(textReportPage.contains(door));
-		assertTrue(textReportPage.contains(postalCode));
-		assertTrue(textReportPage.contains(locality));
+			// check if report page includes the proper values
+			submit.click();
+		//}
 	}
-	
+
 	public void removeCustomer(String vat) throws IOException {
-		HtmlAnchor removeCustomerLink = page.getAnchorByHref("RemoveCustomerPageController");
-		HtmlPage nextPage = (HtmlPage) removeCustomerLink.openLinkInNewWindow();
-		assertTrue(nextPage.asText().contains(vat));
-		
-		HtmlForm removeCustomerForm = nextPage.getForms().get(0);
-		HtmlInput vatInput = removeCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(vat);
-		HtmlInput submit = removeCustomerForm.getInputByName("submit");
-		submit.click();
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage nextPage = webClient.getPage(APPLICATION_URL + "RemoveCustomerPageController");
+			assertTrue(nextPage.asText().contains(vat));
+
+			HtmlForm removeCustomerForm = nextPage.getForms().get(0);
+			HtmlInput vatInput = removeCustomerForm.getInputByName("vat");
+			vatInput.setValueAttribute(vat);
+			HtmlInput submit = removeCustomerForm.getInputByName("submit");
+			submit.click();
+		//}
 	}
-	
+
 	public HtmlTable getCustomerAddresses(String vat) throws IOException {
-		HtmlAnchor findCustomerLink = page.getAnchorByHref("getCustomerByVAT.html");
-		HtmlPage nextPage = (HtmlPage) findCustomerLink.openLinkInNewWindow();
-		// get the page first form:
-		HtmlForm findCustomerForm = nextPage.getForms().get(0);
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage nextPage = webClient.getPage(APPLICATION_URL + "getCustomerByVAT.html");
+			// get the page first form:
+			HtmlForm findCustomerForm = nextPage.getForms().get(0);
 
-		// place data at form
-		HtmlInput vatInput = findCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(vat);
-		// submit form
-		HtmlInput submit = findCustomerForm.getInputByName("submit");
-		HtmlPage reportPage = submit.click();
-		System.out.println(reportPage.asText());
-		return null;//TODO
+			// place data at form
+			HtmlInput vatInput = findCustomerForm.getInputByName("vat");
+			vatInput.setValueAttribute(vat);
+			
+			// submit form
+			HtmlInput submit = findCustomerForm.getInputByName("submit");
+			HtmlPage reportPage = submit.click();
+			HtmlTable table = (HtmlTable) reportPage.getElementById("addresses");
+			return table;
+		//}
 	}
 	
+	public HtmlTable getCustomers() throws IOException {
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage page = webClient.getPage(APPLICATION_URL + "GetAllCustomersPageController");	
+			final HtmlTable table = page.getHtmlElementById("clients");
+			return table;
+		//}
+	}
+
 	public void addSale(String vat) throws IOException {
-		// get a specific link
-		HtmlAnchor addSaleLink = page.getAnchorByHref("addSale.html");
-		// click on it
-		HtmlPage nextPage = (HtmlPage) addSaleLink.openLinkInNewWindow();
-		// check if title is the one expected
-		assertEquals("New Sale", nextPage.getTitleText());
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage nextPage = webClient.getPage(APPLICATION_URL + "addSale.html");
+			// check if title is the one expected
+			assertEquals("New Sale", nextPage.getTitleText());
 
-		// get the page first form:
-		HtmlForm addSaleForm = nextPage.getForms().get(0);
+			// get the page first form:
+			HtmlForm addSaleForm = nextPage.getForms().get(0);
 
-		// place data at form
-		HtmlInput vatInput = addSaleForm.getInputByName("customerVat");
-		vatInput.setValueAttribute(vat);
-		
-		// submit form
-		HtmlInput submit = addSaleForm.getInputByValue("Add Sale");
+			// place data at form
+			HtmlInput vatInput = addSaleForm.getInputByName("customerVat");
+			vatInput.setValueAttribute(vat);
 
-		// check if report page includes the proper values
-		submit.click();
+			// submit form
+			HtmlInput submit = addSaleForm.getInputByValue("Add Sale");
+
+			// check if report page includes the proper values
+			submit.click();
+		//}
+	}
+
+	public HtmlTable getCustomerSales(String vat) throws IOException {
+		//try(WebClient webClient = new WebClient()){
+			HtmlPage nextPage = webClient.getPage(APPLICATION_URL + "getCustomerByVAT.html");
+			// get the page first form:
+			HtmlForm findCustomerForm = nextPage.getForms().get(0);
+
+			// place data at form
+			HtmlInput vatInput = findCustomerForm.getInputByName("vat");
+			vatInput.setValueAttribute(vat);
+			
+			// submit form
+			HtmlInput submit = findCustomerForm.getInputByName("submit");
+			HtmlPage reportPage = submit.click();
+			System.out.println(reportPage.asText());
+			return (HtmlTable) reportPage.getElementById("addresses");
+		//}
 	}
 
 
