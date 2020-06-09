0a1f4b5e54e347b18f3caa0586333c656c3b239c
M44TRIX <joaodavid512@gmail.com>
2020-06-09 22:47:58 +0100

    moved vat, phones etc to global constant

--- vvs_webapp/vvs_webapp/src/test/java/htmlunit/HtmlUnitTest.java
+++ vvs_webapp/vvs_webapp/src/test/java/htmlunit/HtmlUnitTest.java
@@ -21,6 +21,24 @@ public class HtmlUnitTest {
 
 	private WebClient webClient;
 	private TestUtils utils;
+	
+	private static final String VAT_1 = "244377090";
+	private static final String DESIG_1 = "Customer 1";
+	private static final String PHONE_1 = "910576931";
+	
+	private static final String VAT_2 = "217485367";
+	private static final String DESIG_2 = "Customer 2";
+	private static final String PHONE_2 = "960872610";
+	
+	private static final String ADDRESS_1 = "Rua Augusta";
+	private static final String DOOR_1 = "9";
+	private static final String POSTAL_CODE_1 = "1100-048";
+	private static final String LOCALITY_1 = "Lisboa";
+	
+	private static final String ADDRESS_2 = "Rua de São João";
+	private static final String DOOR_2 = "1";
+	private static final String POSTAL_CODE_2 = "4150-385";
+	private static final String LOCALITY_2 = "Porto";
 
 	@Before
 	public void setUpClass() throws Exception {
@@ -43,146 +61,118 @@ public class HtmlUnitTest {
 	@Test
 	public void narrativeA() throws IOException {
 		//Set Up
-		final String vat = "244377090";
-		final String desig = "narrativeA";
-		final String phone = "910576931";
-		utils.addCustomer(vat, desig, phone);
+		utils.addCustomer(VAT_1, DESIG_1, PHONE_1);
 		//State of the table before adding the 2 new addresses
-		final HtmlTable tableBefore = utils.getCustomerAddresses(vat);
+		final HtmlTable tableBefore = utils.getCustomerAddresses(VAT_1);
 		final int numRowsBefore = tableBefore==null ? 1 : tableBefore.getRowCount();
 		//Adding the 2 addresses
-		final String address1 = "Rua Augusta";
-		final String door1 = "9";
-		final String postalCode1 = "1100-048";
-		final String locality1 = "Lisboa";
-		utils.addAddress(vat, address1, door1, postalCode1, locality1);
-		final String address2 = "Rua de São João";
-		final String door2 = "1";
-		final String postalCode2 = "4150-385";
-		final String locality2 = "Porto";
-		utils.addAddress(vat, address2, door2, postalCode2, locality2);
+		utils.addAddress(VAT_1, ADDRESS_1, DOOR_1, POSTAL_CODE_1, LOCALITY_1);
+		utils.addAddress(VAT_1, ADDRESS_2, DOOR_2, POSTAL_CODE_2, LOCALITY_2);
 		//State of the table after adding the 2 new addresses
-		final HtmlTable tableAfter = utils.getCustomerAddresses(vat);
+		final HtmlTable tableAfter = utils.getCustomerAddresses(VAT_1);
 		final int numRowsAfter = tableAfter.getRowCount();
 		//Verify row count increased by 2
 		assertEquals(2, numRowsAfter-numRowsBefore);
 		//Verify that the table of addresses includes the new ones
 		final HtmlTableRow fstAddedRow = tableAfter.getRow(numRowsAfter-2);
-		assertEquals(address1, fstAddedRow.getCell(1).asText());
-		assertEquals(door1, fstAddedRow.getCell(2).asText());
-		assertEquals(postalCode1, fstAddedRow.getCell(3).asText());
-		assertEquals(locality1, fstAddedRow.getCell(4).asText());
+		assertEquals(ADDRESS_1, fstAddedRow.getCell(1).asText());
+		assertEquals(DOOR_1, fstAddedRow.getCell(2).asText());
+		assertEquals(POSTAL_CODE_1, fstAddedRow.getCell(3).asText());
+		assertEquals(LOCALITY_1, fstAddedRow.getCell(4).asText());
 		final HtmlTableRow sndAddedRow = tableAfter.getRow(numRowsAfter-1);
-		assertEquals(address2, sndAddedRow.getCell(1).asText());
-		assertEquals(door2, sndAddedRow.getCell(2).asText());
-		assertEquals(postalCode2, sndAddedRow.getCell(3).asText());
-		assertEquals(locality2, sndAddedRow.getCell(4).asText());
+		assertEquals(ADDRESS_2, sndAddedRow.getCell(1).asText());
+		assertEquals(DOOR_2, sndAddedRow.getCell(2).asText());
+		assertEquals(POSTAL_CODE_2, sndAddedRow.getCell(3).asText());
+		assertEquals(LOCALITY_2, sndAddedRow.getCell(4).asText());
 		// Tear down
-		utils.removeCustomer(vat);
+		utils.removeCustomer(VAT_1);
 	}
 
 	@Test
 	public void narrativeB() throws IOException {
-		String vat1 = "244377090";
-		String desig1 = "narrativeB-1";
-		String phone1 = "910576931";
-		String vat2 = "217485367";
-		String desig2 = "narrativeB-2";
-		String phone2 = "960872610";
 		int confirmedInfoCount = 0;
 		//-------------before---------
 		final HtmlTable tableBefore = utils.getCustomers();
 		final int countBefore = tableBefore==null ? 1 : tableBefore.getRowCount();
 		//------------add customers---------
-		utils.addCustomer(vat1, desig1, phone1);
-		utils.addCustomer(vat2, desig2, phone2);
+		utils.addCustomer(VAT_1, DESIG_1, PHONE_1);
+		utils.addCustomer(VAT_2, DESIG_2, PHONE_2);
 		//-------------after---------
 		final HtmlTable tableAfter = utils.getCustomers();
 		int countAfter = tableAfter.getRowCount();
 		for (final HtmlTableRow row : tableAfter.getRows()) {
-			if (row.getCell(2).asText().equals(vat1)) {
-				assertEquals(desig1, row.getCell(0).asText());
-				assertEquals(phone1, row.getCell(1).asText());
-				assertEquals(vat1, row.getCell(2).asText());
+			if (row.getCell(2).asText().equals(VAT_1)) {
+				assertEquals(DESIG_1, row.getCell(0).asText());
+				assertEquals(PHONE_1, row.getCell(1).asText());
+				assertEquals(VAT_1, row.getCell(2).asText());
 				confirmedInfoCount++;
-			} else if (row.getCell(2).asText().equals(vat2)) {
-				assertEquals(desig2, row.getCell(0).asText());
-				assertEquals(phone2, row.getCell(1).asText());
-				assertEquals(vat2, row.getCell(2).asText());
+			} else if (row.getCell(2).asText().equals(VAT_2)) {
+				assertEquals(DESIG_2, row.getCell(0).asText());
+				assertEquals(PHONE_2, row.getCell(1).asText());
+				assertEquals(VAT_2, row.getCell(2).asText());
 				confirmedInfoCount++;
 			}
 		}
 		assertEquals(2, confirmedInfoCount);
 		assertTrue((countAfter - countBefore) == confirmedInfoCount);
 		// Tear down
-		utils.removeCustomer(vat1);
-		utils.removeCustomer(vat2);
+		utils.removeCustomer(VAT_1);
+		utils.removeCustomer(VAT_2);
 	}
 
 	@Test
 	public void narrativeC() throws IOException {		
 		//Set Up
-		final String vat = "244377090";
-		final String desig = "narrativeC";
-		final String phone = "910576931";
-		utils.addCustomer(vat, desig, phone);
+		utils.addCustomer(VAT_1, DESIG_1, PHONE_1);
 		//Add sale
-		utils.addSale(vat);
-		final HtmlTable tableAfter = utils.getCustomerSales(vat);
+		utils.addSale(VAT_1);
+		final HtmlTable tableAfter = utils.getCustomerSales(VAT_1);
 		int indexLatest = tableAfter.getRows().size() - 1;
 		HtmlTableRow row = tableAfter.getRow(indexLatest);
 		assertEquals("O", row.getCell(3).asText());
 		// Tear down
-		utils.removeCustomer(vat);
+		utils.removeCustomer(VAT_1);
 	}
 
 	@Test
 	public void narrativeD() throws IOException {		
 		//Set Up
-		final String vat = "244377090";
-		final String desig = "narrativeD";
-		final String phone = "910576931";
-		utils.addCustomer(vat, desig, phone);
-		utils.addSale(vat);
+		utils.addCustomer(VAT_1, DESIG_1, PHONE_1);
+		utils.addSale(VAT_1);
 		//Close the sale		
-		final HtmlTable table = utils.getCustomerSales(vat);
+		final HtmlTable table = utils.getCustomerSales(VAT_1);
 		int indexLatest = table.getRows().size() - 1;
 		HtmlTableRow row = table.getRow(indexLatest);		
 		utils.closeSale(row.getCell(0).asText());
 		//Assert that the sale is closed
-		final HtmlTable tableAfter = utils.getCustomerSales(vat);
+		final HtmlTable tableAfter = utils.getCustomerSales(VAT_1);
 		HtmlTableRow rowAfter = tableAfter.getRow(indexLatest);		
 		assertEquals("C", rowAfter.getCell(3).asText());
 		// Tear down
-		utils.removeCustomer(vat);
+		utils.removeCustomer(VAT_1);
 	}
 	
 	@Test
 	public void narrativeE() throws IOException {
 		// Add Customer
-		final String vat = "229122205";
-		final String desig  = "narrativeE";
-		final String phone = "910576931";
-		HtmlPage addCustomerPage = utils.addCustomer(vat, desig, phone);
+		HtmlPage addCustomerPage = utils.addCustomer(VAT_1, DESIG_1, PHONE_1);
 		String textReportPage = addCustomerPage.asText();
-		assertTrue(textReportPage.contains(desig));
-		assertTrue(textReportPage.contains(phone));
-		//Add address
-		final String address = "Rua Augusta";
-		final String door = "9";
-		final String postalCode = "1100-048";
-		final String locality = "Lisboa";		
-		HtmlPage addAddressPage = utils.addAddress(vat, address, door, postalCode, locality);
+		assertTrue(textReportPage.contains(DESIG_1));
+		assertTrue(textReportPage.contains(PHONE_1));
+		
+		//Add address	
+		HtmlPage addAddressPage = utils.addAddress(VAT_1, ADDRESS_1, DOOR_1, POSTAL_CODE_1, LOCALITY_1);
 		HtmlTable tableAddresses = (HtmlTable) addAddressPage.getElementById("addresses");
 		int indexLatestAddresses = tableAddresses.getRows().size() - 1;
 		HtmlTableRow rowAddress = tableAddresses.getRow(indexLatestAddresses);
 		final String addressId = rowAddress.getCell(0).asText();
-		assertEquals(address, rowAddress.getCell(1).asText());
-		assertEquals(door, rowAddress.getCell(2).asText());
-		assertEquals(postalCode, rowAddress.getCell(3).asText());
-		assertEquals(locality, rowAddress.getCell(4).asText());
+		assertEquals(ADDRESS_1, rowAddress.getCell(1).asText());
+		assertEquals(DOOR_1, rowAddress.getCell(2).asText());
+		assertEquals(POSTAL_CODE_1, rowAddress.getCell(3).asText());
+		assertEquals(LOCALITY_1, rowAddress.getCell(4).asText());
+		
 		// Add Sale
-		HtmlPage addASalePage = utils.addSale(vat);
+		HtmlPage addASalePage = utils.addSale(VAT_1);
 		HtmlTable tableSales = (HtmlTable) addASalePage.getElementById("sales");
 		int indexLatestSales = tableSales.getRows().size() - 1;
 		HtmlTableRow row = tableSales.getRow(indexLatestSales);
@@ -191,17 +181,17 @@ public class HtmlUnitTest {
 		assertEquals(formatter.format(LocalDate.now()), row.getCell(1).asText());
 		assertEquals("0.0", row.getCell(2).asText());
 		assertEquals("O", row.getCell(3).asText());
-		assertEquals(vat, row.getCell(4).asText());	
-		//Add Sale Delivery
-		HtmlPage addSaleDeliveryPage = utils.addSaleDelivery(vat, saleId, addressId);		
-		HtmlTable saleDeliTable = (HtmlTable) addSaleDeliveryPage.getElementById("salesDelivery");
+		assertEquals(VAT_1, row.getCell(4).asText());	
 		
+		//Add Sale Delivery
+		HtmlPage addSaleDeliveryPage = utils.addSaleDelivery(VAT_1, saleId, addressId);		
+		HtmlTable saleDeliTable = (HtmlTable) addSaleDeliveryPage.getElementById("salesDelivery");
 		int indexLatestSaleDeliv = saleDeliTable.getRows().size() - 1;
 		HtmlTableRow rowSaleDeli = saleDeliTable.getRow(indexLatestSaleDeliv);
 		assertEquals(saleId, rowSaleDeli.getCell(1).asText());
 		assertEquals(addressId, rowSaleDeli.getCell(2).asText());
 		//Tear down
-		utils.removeCustomer(vat);
+		utils.removeCustomer(VAT_1);
 	}
 	
 	
